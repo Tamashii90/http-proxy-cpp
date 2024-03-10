cd7297c88b9f523bbf29621547604792be8b3c77
Tamashii90 <patoboy90@hotmail.com>
2024-03-10 05:04:47 +0200

    implement basic functionality

--- Makefile
+++ Makefile
@@ -7,8 +7,14 @@ SOURCE = boost.cpp
 boost: $(SOURCE)
 	$(CC) $(SOURCE) -I $(INCLUDE) $(LDFLAGS) -o $@
 
+boost_debug: $(SOURCE)
+	$(CC) $(SOURCE) -I $(INCLUDE) $(LDFLAGS) -g -o $@
+
 run: boost
 	./boost
+
+debug: boost_debug
+	gdb ./boost_debug
 
 clean:
 	rm boost
--- boost.cpp
+++ boost.cpp
@@ -1,49 +1,120 @@
+#include <algorithm>
 #include <boost/asio.hpp>
 #include <iostream>
 #include <istream>
 #include <ostream>
+#include <regex>
 #include <string>
 
 constexpr unsigned PORT = 8000;
 
-using std::string;
+constexpr const char *RED = "\x1B[31m";
+constexpr const char *GREEN = "\x1B[32m";
+constexpr const char *YELLOW = "\x1B[33m";
+constexpr const char *BLU = "\x1B[34m";
+constexpr const char *MAG = "\x1B[35m";
+constexpr const char *CYN = "\x1B[36m";
+constexpr const char *WHT = "\x1B[37m";
+constexpr const char *RESET = "\x1B[0m";
+
 using namespace boost;
 
-void handle(asio::ip::tcp::socket &socket) {
-  std::string response;
-  system::error_code ec;
-  asio::read_until(socket, asio::dynamic_buffer(response), "\r\n\r\n");
-  if (ec && ec.value() != asio::error::eof) {
-    throw boost::system::system_error{ec};
-  }
-  std::cout << response << std::endl;
-}
+std::string parse_field(const std::string &http_header,
+                        std::string &&field_name);
 
-std::string request(std::string host, asio::io_context &io_context) {
-  std::stringstream request_stream;
-  request_stream << "GET / HTTP/1.1\r\n"
-                    "Host: "
-                 << host
-                 << "\r\n"
-                    "Accept: text/html\r\n"
-                    "Accept-Language: en-us\r\n"
-                    "Accept-Encoding: identity\r\n"
-                    "Connection: close\r\n\r\n";
-  const auto request = request_stream.str();
-
+// TODO handle errors in writes and reads (e.g. most writes don't have
+// error_code in their arguments)
+void request(asio::ip::tcp::socket &client_socket, const std::string &host,
+             asio::io_context &io_context, const std::string &request) {
   asio::ip::tcp::resolver resolver{io_context};
-  const auto endpoints = resolver.resolve(host, "http");
+  system::error_code err;
+  const auto endpoints = resolver.resolve(host, "http", err);
+  if (err) {
+    std::cout << RED << err.message() << "\n"
+              << "Host: [" << host << "]" << RESET << std::endl;
+    return;
+  }
   asio::ip::tcp::socket socket{io_context};
   const auto connected_endpoint = asio::connect(socket, endpoints);
 
   asio::write(socket, asio::buffer(request));
-  std::string response;
+
+  std::string response_header_plus;
+  // CAUTION read_until might read past the delimiter, but returns real length
+  auto header_len = asio::read_until(
+      socket, asio::dynamic_buffer(response_header_plus), "\r\n\r\n", err);
+  if (err && err.value() != asio::error::eof) {
+    throw boost::system::system_error{err};
+  }
+
+  // After reading, header_len <= response_header.size() because of potential
+  // extra characters
+  asio::write(client_socket, asio::buffer(response_header_plus, header_len));
+  std::cout << GREEN << response_header_plus.substr(0, header_len) << RESET
+            << std::endl;
+
+  /*****************************************************************
+  // TODO Does this block successfully replace parse_field()?
+  // Do NOT transform everything! There could be parts related to the
+  // response_BODY
+  std::transform(response_header.begin(), response_header.begin() + header_len,
+                 response_header.begin(),
+                 [](auto c) { return std::tolower(c); });
+
+  // Parse content-length field
+  auto len_field_beg =
+      response_header.find("content-length:") + strlen("content-length:");
+  auto len_field_end = response_header.find("\r\n", len_field_beg);
+  auto content_length = stoul(
+      response_header.substr(len_field_beg, len_field_end - len_field_beg));
+  *****************************************************************/
+
+  // TODO handle chunked messages
+  auto content_length = stoul(parse_field(
+      response_header_plus.substr(0, header_len), "content-length"));
+  std::string response_body{response_header_plus.substr(
+      header_len, response_header_plus.size() - header_len)};
+  auto transferred = asio::read(
+      socket, asio::dynamic_buffer(response_body, content_length), err);
+  if (err && err.value() != asio::error::eof) {
+    throw boost::system::system_error{err};
+  }
+  asio::write(client_socket, asio::buffer(response_body));
+}
+
+void handle(asio::ip::tcp::socket &socket, asio::io_context &io_context) {
+  // TODO handle requests that have a body (e.g. POST)
+  std::string request_header;
   system::error_code ec;
-  asio::read(socket, asio::dynamic_buffer(response), ec);
+  asio::read_until(socket, asio::dynamic_buffer(request_header), "\r\n\r\n");
   if (ec && ec.value() != asio::error::eof) {
     throw boost::system::system_error{ec};
   }
-  return response;
+  std::cout << YELLOW << request_header << RESET << std::endl;
+  const std::string host = parse_field(request_header, "host");
+  request(socket, host, io_context, request_header);
+}
+
+std::string parse_field(const std::string &http_header_, std::string &&field) {
+  std::string http_header{http_header_};
+  std::transform(http_header.begin(), http_header.end(), http_header.begin(),
+                 [](auto c) { return std::tolower(c); });
+  std::transform(field.begin(), field.end(), field.begin(),
+                 [](auto c) { return std::tolower(c); });
+  // There's a colon at the end of field names
+  auto len_field_beg = http_header.find(field) + field.size() + strlen(":");
+  auto len_field_end = http_header.find("\r\n", len_field_beg);
+  std::string result =
+      http_header.substr(len_field_beg, len_field_end - len_field_beg);
+  // trim both sides
+  if (result.find(" ") != std::string::npos) {
+    result.erase(result.find(" "), result.find_first_not_of(" "));
+  }
+  if (result.find(" ") != std::string::npos) {
+    result.erase(result.begin() + result.find(" "),
+                 result.begin() + result.find_first_not_of(" "));
+  }
+  return result;
 }
 
 int main() {
@@ -58,17 +129,9 @@ int main() {
       acceptor.accept(socket, client_endpoint);
       std::cout << "New client! on port " << client_endpoint.port()
                 << std::endl;
-      handle(socket);
+      handle(socket, io_context);
     }
-  } catch (std::exception e) {
+  } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
-  // try {
-  // const auto response = request("www.google.com", io_context);
-  // std::cout << response << "\n";
-  // }
-  //
-  // catch (system::system_error &se) {
-  // std::cerr << "Error: " << se.what() << std::endl;
-  // }
 }
